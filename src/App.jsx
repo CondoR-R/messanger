443949f9a1828b8129b6b8ca12c87233ace05a7b
Dmitriy Latkin <lda020504@gmail.com>
2025-03-17 18:52:36 +0700

    Version 0.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./pages/layout/Layout";
+import MainPage from "./pages/mainPage/MainPage";
+import LoginPage from "./pages/loginPage/LoginPage";
+import RegistrationPage from "./pages/loginPage/RegistrationPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <MainPage />,
+      },
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "registration",
+        element: <RegistrationPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <Layout />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -3,14 +3,14 @@ import style from "./Layout.module.css";
 import Header from "../../components/header/Header";
 import MainPage from "../mainPage/MainPage";
 import LoginPage from "../loginPage/LoginPage";
+import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
     <div className={style.layout}>
       <Header />
       <main>
-        <LoginPage />
-        {/* <MainPage /> */}
+        <Outlet />
       </main>
     </div>
   );
